17e04e7d1a9b6af24f8d2583e4560afd1a7cd97b
Gulizar Sadykova <43236716+icyvan@users.noreply.github.com>
2025-03-23 18:09:54 +0000

    homework tale.js

---
+++ src/tale.js
@@ -0,0 +1,16 @@
+function kolobok(name) {
+    switch (name) {
+        case 'дедушка':
+            return 'Я от дедушки ушёл'
+        case 'заяц':
+            return 'Я от зайца ушёл'
+        case 'лиса':
+            return 'Меня съели'
+        default:
+            return 'Привет! Я колобок'
+    }
+}
+
+function newYear(name){
+    return `${name}! ${name}! ${name}!`
+}
