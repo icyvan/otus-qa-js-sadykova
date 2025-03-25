887936fd6dfc43de136d7c82cf6207869fe858b2
Gulizar Sadykova <43236716+icyvan@users.noreply.github.com>
2025-03-25 21:19:24 +0000

    unit test homework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended
-]
+];
